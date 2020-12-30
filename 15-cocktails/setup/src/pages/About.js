0c1854a378b1773f3df0ccbe2204ba2e81bdc79d
Loxi <dianyuyi@gmail.com>
2020-12-30 21:18:08 +0800

    Review 08.5
    15 - 1/2

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+        corrupti, recusandae dolorem saepe magnam voluptates pariatur quis ut,
+        maiores in laborum! Suscipit expedita nihil quasi, perferendis eaque
+        quas, quis aliquid nulla ut labore minima ea unde quidem laboriosam
+        tempore atque voluptas quam obcaecati possimus provident.
+      </p>
+      <h1>about us</h1>
+    </section>
+  );
+};
 
-export default About
+export default About;
